c051cc3a2f1db07a55f33d569b84f0fa8681d41d
JesusBriones023 <j.briones.delapuerta@hotmail.com>
2026-02-14 19:34:05 +0100

    Revert "Nueva funcionalidad división"
    This reverts commit e72a5605f8a73f9323d75de6c7466633b042825f.

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,5 @@ public class Main {
         System.out.println("Calculadora Empresa v1");
 	System.out.println("Función suma implementada");
 	System.out.println("Función suma implementada");
-	System.out.println(10/0);
     }
 }
